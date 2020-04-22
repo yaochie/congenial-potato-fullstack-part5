6f9345032674db4226bd09f00d2291a8ef11d6c1
yc <10258156+yaochie@users.noreply.github.com>
2020-04-22 19:19:52 -0400

    exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +13,20 @@ const App = () => {
     )  
   }, [])
 
+  if (user === null) {
+    return (
+      <LoginForm
+        setUser={setUser}
+      />
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>
+        {user.name} logged in
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
